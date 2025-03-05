d471cea596899d0f5fec1ff58a2eb21f9dea91b0
unHappy96 <ya.elikov@yandex.ru>
2025-03-05 23:42:45 +0700

    Создали класс ноутбуков

---
+++ laptop.java
@@ -0,0 +1,30 @@
+public class laptop {
+    private int ram;
+    private int hdd;
+    private String os;
+    private String color;
+
+
+    public laptop(int ram, int hdd, String os, String color) {
+        this.ram = ram;
+        this.hdd = hdd;
+        this.os = os;
+        this.color = color;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getHdd(){
+        return hdd;
+    }
+
+    public String getOs(){
+        return os;
+    }
+
+    public String getColor(){
+        return color;
+    }
+}
