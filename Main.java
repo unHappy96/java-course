765b5bd55a3250758231fd4bf1024f491dccd441
unHappy96 <ya.elikov@yandex.ru>
2025-03-05 23:54:26 +0700

    Переименовали класс Laptop

--- Main.java
+++ Main.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args){
 
         //Создали множество ноутбуков
-        HashSet<laptop> laptops = new HashSet<>();
-        laptops.add(new laptop(2, 80, "Windows XP", "white"));
-        laptops.add(new laptop(4, 512, "Windows 7", "White"));
-        laptops.add(new laptop(8, 1024, "Windows 10", "grey"));
-        laptops.add(new laptop(16, 512, "Windows 11", "blue"));
+        HashSet<Laptop> laptops = new HashSet<>();
+        laptops.add(new Laptop(2, 80, "Windows XP", "white"));
+        laptops.add(new Laptop(4, 512, "Windows 7", "White"));
+        laptops.add(new Laptop(8, 1024, "Windows 10", "grey"));
+        laptops.add(new Laptop(16, 512, "Windows 11", "blue"));
 
         //просто проверка
-        //for (var item : laptops) System.out.println(item.getHdd());
+        for (var item : laptops) System.out.println(item.getHdd());
 
-        
+
     }
 }
